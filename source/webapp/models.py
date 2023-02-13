b4be486707a2e3f9fcb93e3e95824717429861ac
Valentin Kim <valentink@yandex.ru>
2023-02-13 16:44:16 +0600

    в модель добавлен choices

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+choices = [('new', 'Новая'), ('in_progress', 'В процессе'), ('finished', 'Сделано')]
 
 class Article(models.Model):
-    description = models.TextField(max_length=200, null=False, blank=False, verbose_name="Описание")
-    status = models.TextField(max_length=20, null=False, blank=False, verbose_name="Статус")
-    execution_date = models.TextField(max_length=10, null=True, blank=False, verbose_name="Дата исполнения")
+    description = models.CharField(max_length=200, null=False, blank=False, help_text = 'Кратко опишите задачу', verbose_name="Описание")
+    status = models.CharField(max_length=20, choices=choices, default='new', help_text = 'Статус задачи', verbose_name="Статус")
+    execution_date = models.CharField(max_length=10, null=True, blank=False, help_text = 'Введите в формате ГГГГ-ММ-ДД', verbose_name="Дата исполнения")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата и время обновления")
 
